34e5f58851590004f768f1d94cd1b600ee3712a5
GodsuyaTheImmortal <lawtq-wp20@student.tarc.edu.my>
2023-11-27 14:05:11 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hehehe';
+
+  text = "Will you go out with me?"
+
+  changeText(){
+
+    this.text = "Yay, see you on the 30th!"
+
+    const iframe = document.getElementById('gifFrame') as HTMLIFrameElement;
+    if (iframe) {
+      iframe.src = "https://giphy.com/embed/LHZyixOnHwDDy";
+    }
+
+  }
+
+  changeText1(){
+
+    this.text = "Fuck You!"
+
+    const iframe = document.getElementById('gifFrame') as HTMLIFrameElement;
+    if (iframe) {
+      iframe.src = "https://giphy.com/embed/I7p8K5EY9w9dC";
+    }
+
+  }
+
+  moveButton() {
+    const button = document.getElementById('noButton');
+  
+    if (button) {
+      const maxX = 500; // Maximum X value
+      const maxY = 150; // Maximum Y value
+  
+      // Generate random positions within these max values
+      const randomX = Math.random() * maxX;
+      const randomY = Math.random() * maxY;
+  
+      // Set button position
+      button.style.position = 'absolute';
+      button.style.left = `${randomX}px`;
+      button.style.top = `${randomY}px`;
+    }
+  }
+}
